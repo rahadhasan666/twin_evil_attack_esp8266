02c46e668de47acd3d84e74921e455d777d1e0d0
Rahad Hasan <156628666+rahadhasan666@users.noreply.github.com>
2024-02-20 15:15:02 +0600

    Update ESP8266-EvilTwin-Rahad.ino

--- ESP8266-EvilTwin-Rahad.ino
+++ ESP8266-EvilTwin-Rahad.ino
@@ -135,7 +135,7 @@ void handleIndex() {
       int n = WiFi.softAPdisconnect (true);
       Serial.println(String(n));
       WiFi.softAPConfig(IPAddress(192, 168, 4, 1) , IPAddress(192, 168, 4, 1) , IPAddress(255, 255, 255, 0));
-      WiFi.softAP("M1z23R", "deauther");
+      WiFi.softAP("ThirdEye", "rahad666");
       dnsServer.start(53, "*", IPAddress(192, 168, 4, 1));
     }
     return;
